210cb4970853c20674c1dec7357e7f7701655a86
Kenneth Christian Mohr (zbckemoh1) <zbckemoh1@zbc.dk>
2019-12-11 12:01:41 +0100

    Har lavet EIDAL og DBEIDAL.

--- BilWebAPI/Controllers/HomeController.cs
+++ BilWebAPI/Controllers/HomeController.cs
@@ -13,6 +13,10 @@ namespace BilWebAPI.Controllers
             // Hello :)
             ViewBag.Title = "Home Page";
 
+            DBEIDAL db = new DBEIDAL();
+
+            db.SaveEventInfo(3, 88.999999, 88.999999, "BB 33 444");
+
             return View();
         }
     }
---
+++ BilWebAPI/Interfaces/IEIDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilWebAPI.Interfaces
+{
+    interface IEIDAL
+    {
+        void SaveEventInfo(int eventTypeID, double lon, double lat, string userRegistrationNo);
+    }
+}
